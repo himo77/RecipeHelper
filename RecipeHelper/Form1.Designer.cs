783c45700b2f7214a80cf16b7a62634c74588276
himo77 <56316743+himo77@users.noreply.github.com>
2020-09-27 20:04:34 -0500

    Add project files.

---
+++ RecipeHelper/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace RecipeHelper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFahrenheit = new System.Windows.Forms.TextBox();
+            this.txtCups = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtMililitres = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(53, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Celsius";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(118, 43);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(88, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(119, 63);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert to Fahrenheit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fahrenheit";
+            // 
+            // txtFahrenheit
+            // 
+            this.txtFahrenheit.Enabled = false;
+            this.txtFahrenheit.Location = new System.Drawing.Point(118, 201);
+            this.txtFahrenheit.Name = "txtFahrenheit";
+            this.txtFahrenheit.Size = new System.Drawing.Size(100, 20);
+            this.txtFahrenheit.TabIndex = 4;
+            // 
+            // txtCups
+            // 
+            this.txtCups.Enabled = false;
+            this.txtCups.Location = new System.Drawing.Point(408, 201);
+            this.txtCups.Name = "txtCups";
+            this.txtCups.Size = new System.Drawing.Size(100, 20);
+            this.txtCups.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(354, 204);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cups";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(378, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 63);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Convert to Cups";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtMililitres
+            // 
+            this.txtMililitres.Location = new System.Drawing.Point(408, 43);
+            this.txtMililitres.Name = "txtMililitres";
+            this.txtMililitres.Size = new System.Drawing.Size(100, 20);
+            this.txtMililitres.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(332, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Millilitres";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 280);
+            this.Controls.Add(this.txtCups);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtMililitres);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFahrenheit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Recipe Helper";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFahrenheit;
+        private System.Windows.Forms.TextBox txtCups;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtMililitres;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ RecipeHelper/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecipeHelper
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double celsius = 0;
+            //Trying to parse the Celsius text box as a decimal value.
+            if(!double.TryParse(this.txtCelsius.Text, out celsius))
+            {
+                //If the parsing fails, prompting user with error message
+                MessageBox.Show("Please enter a valid numeric value for celsius.", "Conversion Failed");
+            }
+            else
+            {
+                //Converting celsius to fahrenheit
+                double fahrenheit = ((celsius / 5) * 9) + 32;
+                this.txtFahrenheit.Text = fahrenheit.ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double millilitres = 0;
+            //Trying to parse the Millilitres text box as a decimal value.
+            if (!double.TryParse(this.txtMililitres.Text, out millilitres))
+            {
+                //If the parsing fails, prompting user with error message
+                MessageBox.Show("Please enter a valid numeric value for Millilitres.", "Conversion Failed");
+            }
+            else
+            {
+                //Converting millilitres to cups
+                double cups = millilitres / 263.59;
+                this.txtCups.Text = cups.ToString();
+            }
+        }
+    }
+}
